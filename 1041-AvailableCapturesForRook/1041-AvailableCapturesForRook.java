5b5bae182307be9fa8da40a0c34ad04d2c4fc70d
gobika3101 <arunadhar04@gmail.com>
2026-07-09 15:08:26 +0530

    Create 1041-AvailableCapturesForRook

---
+++ 1041-AvailableCapturesForRook/1041-AvailableCapturesForRook.java
@@ -0,0 +1,58 @@
+// Last updated: 7/9/2026, 3:06:20 PM
+class Solution {
+    public int numRookCaptures(char[][] board) {
+        int attack = 0;
+        //to find rook
+        int row =0,col=0;
+        for(int i =0;i<=7;i++){
+            for(int j =0;j<=7;j++){
+                if(board[i][j]=='R'){
+                    row = i;
+                    col = j;
+                    break;
+                }
+            }
+        }
+        //upward
+        int r = row;
+        int c = col;
+        while(r>=0 && board[r][c]!='B'){
+            if(board[r][c]=='p'){
+                attack++;
+                break;
+            }
+            r--;
+        }
+        //downward
+        r = row;
+        c = col;
+        while(r<=7 && board[r][c]!='B'){
+            if(board[r][c]=='p'){
+                attack++;
+                break;
+            }
+            r++;
+        }
+        //left side
+        r = row;
+        c = col;
+        while(c>=0 && board[r][c]!='B'){
+            if(board[r][c]=='p'){
+                attack++;
+                break;
+            }
+            c--;
+        }
+        //rigth side
+        r = row;
+        c = col;
+        while(c<=7 && board[r][c]!='B'){
+            if(board[r][c]=='p'){
+                attack++;
+                break;
+            }
+            c++;
+        }
+        return attack;
+    }
+}
