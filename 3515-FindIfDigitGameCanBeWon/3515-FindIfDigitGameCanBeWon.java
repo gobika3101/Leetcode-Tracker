241e3cf3849e79302f950a450538c9fb4f074db8
gobika3101 <arunadhar04@gmail.com>
2026-07-09 15:06:31 +0530

    Create 3515-FindIfDigitGameCanBeWon

---
+++ 3515-FindIfDigitGameCanBeWon/3515-FindIfDigitGameCanBeWon.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 3:04:25 PM
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int sd =0, dd=0;
+        for (int i=0;i<nums.length;i++){
+            if(nums[i]<10){
+                sd = sd+nums[i];
+            }else{
+                dd = dd+nums[i];
+            }
+        }
+        if (sd!=dd)
+        return true;
+        else
+        return false;
+    }
+}
