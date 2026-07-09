4ba0b4bc191b1276ffeb4252f9ad58ff48695607
gobika3101 <arunadhar04@gmail.com>
2026-07-09 15:08:42 +0530

    Create 695-MaxAreaOfIsland

---
+++ 695-MaxAreaOfIsland/695-MaxAreaOfIsland.java
@@ -0,0 +1,40 @@
+// Last updated: 7/9/2026, 3:06:36 PM
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int fin =0;
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1 && visited[i][j]==false){
+                    int ans =bfs(i,j,visited,grid,m,n);
+                    if(ans>fin) fin=ans;
+                }
+            }
+        }
+        return fin;
+    }
+    int bfs(int i,int j,boolean[][] visited,int[][] grid, int m,int n){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{i,j});
+        visited[i][j]=true;
+        int[][] directions={{-1,0},{1,0},{0,-1},{0,1}};
+        int count=0;
+        while(q.size()>0){
+            int curr[]=q.poll();
+            count++;
+            int x =curr[0];
+            int y =curr[1];
+            for(int k=0;k<4;k++){
+                int nextRow = x+directions[k][0];
+                int nextCol =y+directions[k][1];
+                if(nextRow<m && nextRow>=0 && nextCol<n && nextCol>=0 &&                      visited[nextRow][nextCol]==false && grid[nextRow][nextCol]==1){
+                    q.add(new int[]{nextRow,nextCol});
+                    visited[nextRow][nextCol]=true;
+                }
+            }
+        }
+        return count;
+    }
+}
