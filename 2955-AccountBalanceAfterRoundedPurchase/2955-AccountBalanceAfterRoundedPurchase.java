45893364aeffea44c97b7b54d47c43d25641de00
gobika3101 <arunadhar04@gmail.com>
2026-07-09 15:06:41 +0530

    Create 2955-AccountBalanceAfterRoundedPurchase

---
+++ 2955-AccountBalanceAfterRoundedPurchase/2955-AccountBalanceAfterRoundedPurchase.java
@@ -0,0 +1,6 @@
+// Last updated: 7/9/2026, 3:04:34 PM
+class Solution {
+    public int accountBalanceAfterPurchase(int purchaseAmount) {
+        return 100 -((purchaseAmount + 5) / 10) * 10;
+    }
+}
