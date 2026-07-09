ddb3394c4f6292e4422b96b38ef4a9baf0707e02
gobika3101 <arunadhar04@gmail.com>
2026-07-09 15:06:56 +0530

    Create 2645-PassThePillow

---
+++ 2645-PassThePillow/2645-PassThePillow.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 3:04:50 PM
+class Solution {
+    public int passThePillow(int n, int time) {
+        int cycle = time/(n-1);
+        int rem = time %(n-1);
+        if (cycle%2==0){
+            //Forward
+            return rem+1;
+        }else{
+            //Backward
+            return n - rem;
+        }
+    }
+}
